d11a327ce25fecd36162b5982060f2b176ccf878
McCoyHforschool <mhernandez146@mail.pima.edu>
2024-09-19 10:51:26 -0700

    Update CIS129_lab02_shortStory.py
    Removes the hello world and adds a signature and short story.

--- CIS129_lab02_shortStory.py
+++ CIS129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+#Author : McCoy
+#My first python lab in my cis129 class
+#Prints a simple short story
+print('Once upon a time there was a man who lived in a tall castle.')
+print('The man spent everyday racking his brain for a solution to a major problem he had.')
+print("No matter how hard he thought and thought he couldn't think of anything so he kept on thinking.")
+print('Eventually the man had been thinking so hard and for so long that his brain stopped working')
+print('The man fell over dead and never thought about anything ever again.')
+print('The end')
